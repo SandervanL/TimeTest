c989c690e0f6ead1a4c4c4d12fb88a6de305938c
SandervanL <sannievanl@gmail.com>
2015-05-25 15:46:15 +0200

    Update timeTest.cpp

--- timeTest.cpp
+++ timeTest.cpp
@@ -1,21 +1,21 @@
-#include <stdio.h>
-#include <sys/time.h>
+#include <iostream> //I know printf is less code and may be faster, but there has to be a difference between C and C++ code
+#include <ctime>
 using namespace std;
 
 int main() {
-        long int currentTime;
+        const double milliSecond = (double)CLOCKS_PER_SEC / 1000.0f;
+        clock_t lastTime;
         int times = 0;
-        struct timeval th;
-        gettimeofday(&th, NULL);
-        currentTime = th.tv_usec / 1000;
+        clock_t currentTime;
+        lastTime = clock();
         while (1) {
-                gettimeofday(&th, NULL);
-                if (th.tv_usec / 1000 == currentTime) {
+                if (clock() - lastTime < milliSecond) {
                         times++;
                 }else{
-                        printf("%lu\r", times);
+                        cout << times << "\r";
                         times = 0;
-                        currentTime = th.tv_usec / 1000;
+                        lastTime = clock();
                 }
         }
 }
+
